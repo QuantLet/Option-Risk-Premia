1fbf50d581f3730219916659767f85a67b23f661
Julian <julian.winkel@hu-berlin.de>
2023-09-16 18:56:49 +0200

    IV vs RV

--- pf.py
+++ pf.py
@@ -13,14 +13,15 @@ Get risk-free rate for the time of the instrument!
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import timedelta
 import pdb
 from src.delta_hedging import delta_hedge
 from src.blackscholes import Call
-from src.helpers import assign_groups
+from src.helpers import assign_groups, load_expiration_price_history, compute_vola
 
 
-def analyze_portfolio(dat, week, iv_var_name):
+def analyze_portfolio(dat, week, iv_var_name, expiration_history):
     """
     dat, pd.DataFrame as from main.py
     week, int, week indicator
@@ -34,10 +35,16 @@ def analyze_portfolio(dat, week, iv_var_name):
     #@Todo: Use Rookley to solve this problem. Or reconstruct vola surface using another method, then price all options around the same time for this.
     # Might as well use Rookley at a specific point of the day
     dat = dat.rename(columns = {'spot': 'index_price'})
-    pdb.set_trace()
+
+    # Introduce expiration prices
+    #dat = dat.merge(expiration_history, on ='Date')
+    
     # Min 4 days to maturity
     calls = dat.loc[(dat['is_call'] == 1) & (dat['tau'] * 365 >= 2)]
+    #calls = dat.loc[(dat['is_call'] == 1)]
     print('Calls only')
+
+    calls['instrument_price_on_expiration'] = calls.apply(lambda x: Call.Price(x['expiration_price'], x['strike'], 0, x[iv_var_name], x['tau']), axis = 1)
 
     # First, use BS Call value function to get Dollar Value for Call Parameters
     calls['instrument_price'] = calls.apply(lambda x: Call.Price(x['index_price'], x['strike'], 0, x[iv_var_name], x['tau']), axis = 1)
@@ -61,6 +68,8 @@ def analyze_portfolio(dat, week, iv_var_name):
     final_instrument_price_dct = {}
     initial_instrument_price_dct = {}
     initial_tau_dct = {}
+    start_date_dct = {}
+    end_date_dct = {}
 
 
     counter = 0
@@ -70,6 +79,9 @@ def analyze_portfolio(dat, week, iv_var_name):
         sub = calls.loc[idx]
         sub['future_position'] = None
         print(sub.head())
+
+        #if sub.shape[0] <= 1:
+        #    pdb.set_trace()
 
         # Take last price per day
         #last_timestamp_per_day = sub.groupby('time')['timestamp'].max()
@@ -103,7 +115,9 @@ def analyze_portfolio(dat, week, iv_var_name):
         n_shares_dct[instrument], cost_of_shares_dct[instrument], cumulative_cost_dct[instrument], interest_cost_dct[instrument] = delta_hedge(daily['delta'].to_list(), daily['index_price'].to_list(), 0.01, daily['tau'].iloc[0])
         delta_hedge_cost = cumulative_cost_dct[instrument][-1]
         initial_instrument_price = daily.iloc[0]['instrument_price']
-        final_instrument_price = daily.iloc[-1]['instrument_price']
+
+        #@Todo: Still have to estimate instrument price from expiration price
+        final_instrument_price = daily.iloc[-1]['instrument_price_on_expiration']
         initial_tau = daily.iloc[0]['tau']
 
 
@@ -112,6 +126,8 @@ def analyze_portfolio(dat, week, iv_var_name):
         initial_instrument_price_dct[instrument] = initial_instrument_price
         final_instrument_price_dct[instrument] = final_instrument_price
         initial_tau_dct[instrument] = initial_tau
+        start_date_dct[instrument] = daily.iloc[0]['day']
+        end_date_dct[instrument] = daily.iloc[-1]['day']
 
         # @Todo also have to consider the payoff of the long call here!!
         # Delta hedge cost is negative if we made a profit in the hedge position!
@@ -143,14 +159,21 @@ def analyze_portfolio(dat, week, iv_var_name):
 
     # n_shares_dct, cost_of_shares_dct, cumulative_cost_dct, interest_cost_dct, 
     # 'n_shares', 'cost_of_shares', 'cumulative_cost', 'interest_cost',
-    perf_overview = pd.DataFrame(data = [pnl,  initial_instrument_price_dct, final_instrument_price_dct, initial_tau_dct], index = ['pnl',  'initial_instrument_price', 'final_instrument_price', 'tau']).T
+    perf_overview = pd.DataFrame(data = [pnl,  initial_instrument_price_dct, final_instrument_price_dct, initial_tau_dct, start_date_dct, end_date_dct], index = ['pnl',  'initial_instrument_price', 'final_instrument_price', 'tau', 'start_date', 'end_date']).T
     perf_overview['ndays'] = round(perf_overview['tau'] * 365)
     perf_overview['tau_rounded'] = round(perf_overview['tau'], 2)
 
     grp = perf_overview.groupby('ndays')['pnl']
     print(grp.describe())
 
-    perf_overview.loc[perf_overview['ndays'] == 9].describe()
+
+    # @Todo: Now relate this plot to the IV over Realized Vola premium!!
+    plt.plot(pd.to_datetime(perf_overview['start_date']), perf_overview['pnl'])
+    plt.ylim(-5000, 5000)
+    plt.show()
+    
+    
+    #perf_overview.loc[perf_overview['ndays'] == 9].describe()
 
     # Instead: Add all None's in the last row if daily data frame, then add the columns
 
@@ -159,14 +182,19 @@ def analyze_portfolio(dat, week, iv_var_name):
     # Check Jackwerth
 
     #@Todo: 
-    # 1) Performance over Time
+    # 1) Performance over Time (use start date and end date)
     # 2) Relate this to IV vs Realized Vola Premium
-    # 3) 
+    # 3) Take actual expiration price for final instrument price instead of an estimation!!!
+    # 4) For #1, Calculate Mean IV and Realized Variance!
 
     return pnl_df
 
 if __name__ == '__main__':
 
+    # Load Expiration Price History
+    expiration_price_history = load_expiration_price_history()
+
+    # Load Fitted Data from main.py
     dat = pd.read_csv('out/fitted_data.csv')#pd.read_csv('data/option_transactions.csv')
     dat['date'] = pd.to_datetime(dat['day'])
 
@@ -176,6 +204,41 @@ if __name__ == '__main__':
     
     dat.sort_values('day', inplace = True)
 
+    # Merge Expiration Prices to Transactions
+    #pdb.set_trace()
+    #window_size = 2
+    dat = dat.merge(expiration_price_history, on ='Date')
+    vola_df = dat.copy(deep=True)
+
+    # IV vs RV for fixed amount of days
+    for i in range(10):
+        daily_volas = vola_df.loc[round(vola_df['tau'] * 365) == i]
+        if daily_volas.shape[0] > 10:
+            avg_daily_vola = daily_volas.groupby('day')['rookley_predicted_iv', 'expiration_price'].mean().reset_index()
+            avg_daily_vola['rolling_iv'] = avg_daily_vola['rookley_predicted_iv']#.mean() #.rolling(window_size).mean()# * (365/window_size)
+            avg_daily_vola['rolling_rv'] = avg_daily_vola['expiration_price'].pct_change().rolling(2).std() * (365)**0.5 #compute_vola(avg_daily_vola['expiration_price'], 1) 
+            avg_daily_vola['date'] = pd.to_datetime(avg_daily_vola['day'])
+
+            fig = plt.figure(figsize = (10,7))
+            plt.plot(avg_daily_vola['date'], avg_daily_vola['rolling_iv'], label = 'IV')
+            plt.plot(avg_daily_vola['date'], avg_daily_vola['rolling_rv'], label = 'RV')
+            plt.title('Days to Maturity: ' + str(i))
+            plt.legend()
+            plt.savefig('plots/iv_vs_rv_ndays=' + str(i) + '.png')
+            #vola_df['rolling_daily_real_vola'] = compute_vola(avg_daily_vola['rookley_predicted_iv'], window_size)
+
+            
+            avg_daily_vola['voldiff'] = avg_daily_vola['rolling_iv'] - avg_daily_vola['rolling_rv']
+            print(avg_daily_vola['voldiff'].describe())
+
+            fig = plt.figure(figsize = (10,7))
+            plt.plot(avg_daily_vola['date'], avg_daily_vola['voldiff'])
+            plt.title('Days to Maturity: ' + str(i))
+            plt.legend()
+            plt.savefig('plots/voladiff_ndays=' + str(i) + '.png')
+            
+
+    pdb.set_trace()
     # Assess difference between Rookley and simple Regression estimated IV
     dat['ivdiff_abs'] = dat['rookley_predicted_iv'] - dat['predicted_iv']
     dat['ivdiff_rel'] = dat['ivdiff_abs'] / dat['rookley_predicted_iv']
@@ -196,7 +259,7 @@ if __name__ == '__main__':
     #dat = dat.loc[pd.notna(dat['rookley_predicted_iv'])]
     #rook = analyze_portfolio(dat, 'all', 'rookley_predicted_iv')
     #pdb.set_trace()
-    test = analyze_portfolio(dat, 'all', 'predicted_iv')
+    test = analyze_portfolio(dat, 'all', 'predicted_iv', expiration_history = expiration_price_history)
     
     pdb.set_trace()
     pnl_per_group = {}
