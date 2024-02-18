cdf563f2e437b86fa74d538fe47826dbbcc21b17
Julian <julian.winkel@hu-berlin.de>
2024-02-18 20:19:14 +0100

    more stats in compare.py

--- compare.py
+++ compare.py
@@ -7,9 +7,43 @@ import matplotlib.pyplot as plt
 import pdb
 import time
 import csv
+import statsmodels.api as sm
+import pylab
 from scipy import stats 
 from scipy.stats import ttest_1samp
 from pathlib import Path
+
+def QQ_plot(data, nn, fname):
+    # Source: https://stackoverflow.com/questions/13865596/quantile-quantile-plot-using-scipy
+    fig = plt.figure(figsize = (10,7))
+
+    # Sort as increasing
+    y = np.sort(data)
+    
+    # Compute sample mean and std
+    mean, std = np.mean(y), np.std(y)
+    
+    # Compute set of Normal quantiles
+    ppf = stats.t(df = nn, loc = mean, scale = std).ppf
+    #ppf = stats.norm(loc=mean, scale=std).ppf # Inverse CDF
+    N = len(y)
+    x = [ppf( i/(N+2) ) for i in range(1,N+1)]
+
+    # Make the QQ scatter plot
+    plt.scatter(x, y)
+    
+    # Plot diagonal line
+    dmin, dmax = np.min([x,y]), np.max([x,y])
+    diag = np.linspace(dmin, dmax, 1000)
+    plt.plot(diag, diag, color='red', linestyle='--')
+    plt.gca().set_aspect('equal')
+    
+    # Add labels
+    plt.xlabel('Normal quantiles')
+    plt.ylabel('Sample quantiles')
+    plt.show()
+    #plt.savefig(fname)
+
 
 def ttest(dat, out_path, min_moneyness = 0.7, max_moneyness = 1.3):
     """
@@ -20,6 +54,11 @@ def ttest(dat, out_path, min_moneyness = 0.7, max_moneyness = 1.3):
     Path(out_path).mkdir(parents=True, exist_ok=True)
     pdct = {}
     df = dat.copy(deep = True)
+
+    # Make up some dummy data and test
+    #xx = np.random.normal(loc=5.0, scale=0.5, size=1000)
+    #QQ_plot(xx)
+
     for mn in df['moneyness'].sort_values().unique():
         if mn >= min_moneyness and mn <= max_moneyness:
 
@@ -37,6 +76,13 @@ def ttest(dat, out_path, min_moneyness = 0.7, max_moneyness = 1.3):
             print("P-Value: ", p_value)
             pdct[mn] = p_value
             #time.sleep(3)
+            
+            #pdb.set_trace()
+            # Save QQ plot
+            #yy = df['combined_ret_daily']
+            #yw = stats.mstats.winsorize(yy, limits = (0.1, 0.1))
+            #QQ_plot(yy, yy.shape[0], out_path + '/___QQ___moneyness>=' + str(mn) + '&moneyness<=' + str(higher) + '.png')
+            #QQ_plot(yy, yy.shape[0], out_path + '/___QQ___moneyness>=' + str(mn) + '&moneyness<=' + str(higher) + '.png')
 
     print(pdct)
 
@@ -48,6 +94,25 @@ def ttest(dat, out_path, min_moneyness = 0.7, max_moneyness = 1.3):
     plt.title('n = ' + str(sub.shape[0]))
     plt.xlim((-1, 1))
     plt.savefig(out_path + '/moneyness_between_95_and_105' + '.png')
+
+    """
+    pdb.set_trace()
+
+    # QQ Plots
+    vec = df['combined_ret_daily']
+    vec_standardized = (vec - vec.mean()) / vec.std()
+    #measurements = stats.t.rvs(vec_standardized.to_list(), loc=0, scale=1, size=sub.shape[0])
+    #measurements = np.random.normal(loc = 20, scale = 5, size=100)  
+    fig = plt.figure(figsize = (10, 7))  
+    sm.qqplot(vec_standardized, line='45')
+    #stats.probplot(vec_standardized, dist="norm", plot=pylab)
+    plt.ylim(-4, 4)
+    pylab.show()
+    
+    # Working test for T-distribution
+    #sm.qqplot(vec_standardized, stats.t, distargs=(vec.shape[0],), loc = vec.mean(), scale = vec.std())
+    """
+
 
     with open(out_path + '/t-tests.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
         w = csv.DictWriter(f, pdct.keys())
@@ -82,9 +147,15 @@ for df in [vanilla_fee, vanilla_no_fee, crash_resistant_fee, crash_resistant_no_
     df.replace([np.inf, -np.inf], np.nan, inplace=True)
     df.dropna(subset=["combined_payoff_daily", "combined_ret_daily"], how="all", inplace=True)
 
-vanilla_fee[['combined_payoff']].describe()
-vanilla_no_fee[['combined_payoff']].describe()
+#print(vanilla_fee[['combined_ret_daily']].describe())
+#print(vanilla_no_fee[['combined_ret_daily']].describe())
+#print(crash_resistant_fee[['combined_ret_daily']].describe())
+#print(crash_resistant_no_fee[['combined_ret_daily']].describe())
 
+print(vanilla_fee.loc[(vanilla_fee['moneyness'] >= 0.95) & (vanilla_fee['moneyness'] <= 1.05)][['combined_ret_daily']].describe())
+print(crash_resistant_fee.loc[(crash_resistant_fee['moneyness'] >= 0.95) & (crash_resistant_fee['moneyness'] <= 1.05)][['combined_ret_daily']].describe())
+
+pdb.set_trace()
 # Test significance
 p1_values = ttest(vanilla_no_fee, out_path = base + 'out/vanilla/no_fees/density')
 p2_values = ttest(vanilla_fee, out_path = base + 'out/vanilla/fees/density')
@@ -93,6 +164,7 @@ p4_values = ttest(crash_resistant_no_fee, out_path = base + 'out/crash_resistant
 
 pv = pd.DataFrame({'Vanilla ex Fee': p1_values, 'Vanilla cum Fee': p2_values, 'Crash Resistant ex Fee': p3_values, 'Crash Resistant cum Fee': p4_values})
 pv.round(4).to_csv(base + 'out/pvalues.csv')
+
 
 pdb.set_trace()
 vanilla_fee.loc[vanilla_fee['days_to_maturity'] <= 20].groupby('days_to_maturity')['combined_payoff'].describe()
